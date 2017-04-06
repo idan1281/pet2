5de4939a20da7e88e5c13604b92a62fa152acf3a
idan1281 <idan1281@gmail.com>
2017-04-06 16:50:13 +0300

    restore the file

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -6,16 +6,14 @@ cp /var/lib/jenkins/.m2/repository/org/springframework/samples/spring-petclinic/
 cat <<EOF > /tmp/docker/Dockerfile
 FROM tomcat:8.0
 MAINTAINER "idan mishor <Idan1281@gmail.com>"/
-COPY ./target/*.war /usr/local/tomcat/webapps/
+ADD ./*.war /usr/local/tomcat/webapps/
 EOF
 
 # create the docker image
 cd /tmp/docker
-docker build -t idan1281/petclinic -t idan1281/petclinic:latest_new .
+docker build -t idan1281/petclinic:${JOB_NAME}${BUILD_NUMBER}  -t idan1281/petclinic:latest .
 
 # upload the docker image to docker hub
-#docker login -u=idan1281 -p=01021981
-#docker push idan1281/petclinic:${JOB_NAME}${BUILD_NUMBER}
-#docker push idan1281/petclinic:latest
-
-
+docker login -u=idan1281 -p=01021981
+docker push idan1281/petclinic:${JOB_NAME}${BUILD_NUMBER}
+docker push idan1281/petclinic:latest
